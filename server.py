4475bb3a87cdce19fea03093de2eea7884441a4a
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-03-20 13:03:58 +0530

    Final

--- server.py
+++ server.py
@@ -1,27 +1,53 @@
+from flask import Flask, jsonify, redirect, request
 import requests
 import time
+import random
+import threading
+from flask_cors import CORS  # Import CORS module
 
-def fetch_server_data():
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+# List of backend servers
+backend_servers = [
+    "https://example.com",
+    "https://facebook.com",
+    "https://instagram.com",
+    "https://jsonplaceholder.typicode.com",
+    "https://api.publicapis.org",
+    "https://dog.ceo/api/breeds/list/all",
+]
+
+# Dictionary to store server response times
+server_response_times = {server: 0 for server in backend_servers}
+
+def get_server_response(server):
+    start_time = time.time()
     try:
-        response = requests.get("http://localhost:8081")  # Assuming the Go program is running locally
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Failed to fetch server data. Status Code:", response.status_code)
-            return None
+        response = requests.get(server)
+        time_to_connect = round((time.time() - start_time) * 1000, 2)  # Convert to milliseconds
+        server_response_times[server] = time_to_connect
     except requests.RequestException as e:
-        print("Error fetching server data:", e)
-        return None
+        server_response_times[server] = -1  # Indicates connection failure
 
-def display_server_data(server_data):
-    if server_data:
-        print("[Server Health Status]")
-        for server in server_data:
-            status = "alive" if server['alive'] else "not alive"
-            print(f"Server {server['address']} is {status}. Connection Time: {server['connection_time']}")
+def update_server_stats():
+    while True:
+        for server in backend_servers:
+            get_server_response(server)
+        time.sleep(3)  # Update server stats every 3 seconds
+
+@app.route("/")
+def proxy_server():
+    server = random.choice(backend_servers)
+    return redirect(server)
+
+@app.route("/stats")
+def server_stats():
+    return jsonify(server_response_times)
 
 if __name__ == "__main__":
-    while True:
-        server_data = fetch_server_data()
-        display_server_data(server_data)
-        time.sleep(5)  # Fetch server data every 5 seconds
+    update_thread = threading.Thread(target=update_server_stats)
+    update_thread.daemon = True
+    update_thread.start()
+
+    app.run(port=8081)
